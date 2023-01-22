74aeabe4e7584b09814bffef92437670230216d0
ThuwaarahanRagunathan <thuwaarahanragunathan@gmail.com>
2023-01-22 20:46:34 +0530

    First E2E commit

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# FirstE2EProject
+
+# Boston E2E project 
+
+### Software And Tools Used
+
+1 [Github Account](https://github.com)
+
+
+Create a new environment 
+
+'''
+For Anaconda 
+To create
+conda create -p venv python==3.7 -y
+To activate 
+conda activate venv\
+'''
+
+To install a file use reqirements.txt file 
+to install use pip install -r requirments.txt
+
+To make connect to git
+git config --global user.name "Name"
+git config --global user.email "emailId"
+
+git add FileName -- for single file 
+git add . --for all files 
+git status  
+git commit -m"Message"
+
+git push origin main
---
+++ app.py
@@ -0,0 +1,39 @@
+import pickle
+from flask import Flask,request,app,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+## Loaded the model
+model = pickle.load(open("LRModel.pkl",'rb'))
+scaler = pickle.load(open("scaling.pkl",'rb'))
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api',methods=["POST"])
+def predict_api():
+    data = request.json["data"]
+    print(data)
+    print(data.values())
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    print(new_data)
+    output = model.predict(new_data)
+    print(output)
+    print(output[0])
+    return jsonify(output[0])
+
+@app.route('/predict',methods = ["POST"])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_data = scaler.transform(np.array(data).reshape(1,-1))
+    print(data)
+    print(final_data)
+    output = model.predict(final_data)[0]
+    print(output)
+    return render_template("home.html",prediction_text = "The predicted price is {}".format(output))
+
+
+if __name__=="__main__":
+     app.run(debug=True)
